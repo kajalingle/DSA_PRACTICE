9037fb0661a4ad6e0b577f25eb3da82ca1b2be7b
Kajal Ingle <kajalingle627@gmail.com>
2026-09-07 23:47:03 +0530

    input output functions

---
+++ src/dsa/javabasicsquestions/InputOutput.java
@@ -0,0 +1,12 @@
+package dsa.javabasicsquestions;
+
+import java.util.Scanner;
+
+public class InputOutput {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n=sc.nextInt();
+        System.out.println("Entered Number is: "+n);
+    }
+}
